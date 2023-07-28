3ce8fa4876b9ae31cee260661aa0da49c202a8fe
Syed Saif Hossain <75739759+SyedSaifHossain@users.noreply.github.com>
2023-07-28 16:56:42 +0600

    Add files via upload

---
+++ PHP Blog Project/contact.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once("./header.php");
+
+?>
+
+<h1>Login the application</h1>
+<?php
+include_once("./footer.php");
+?>
---
+++ PHP Blog Project/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once("./header.php");
+
+include_once("./hero.php");
+
+include_once("./blog.php");
+
+
+?>
+
+
+<?php include_once("./footer.php"); ?>
